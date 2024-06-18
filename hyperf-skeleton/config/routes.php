bec242c54b5c6157d5181965e140e73e0b9db8dc
Junior-Shyko <franciscoanto@gmail.com>
2024-06-18 13:23:51 -0300

    Commit Inicial

---
+++ hyperf-skeleton/config/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+use App\Controller\UserController;
+use Hyperf\HttpServer\Router\Router;
+
+Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
+
+Router::get('/favicon.ico', function () {
+    return '';
+});
+
+Router::post('create-user', [UserController::class, 'index'] );
