c64742f071490e35e4a1d7f381206ac1267dc42f
Junior-Shyko <franciscaoanto@gmail.com>
2024-06-20 17:03:14 -0300

    :construction: Criado nova migration e novo model

--- hyperf-skeleton/app/Controller/OpportunityController.php
+++ hyperf-skeleton/app/Controller/OpportunityController.php
@@ -7,11 +7,39 @@ namespace App\Controller;
 use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\HttpServer\Contract\ResponseInterface;
 use App\Model\AuditAction;
-use Hyperf\DbConnection\Db;
+use App\Model\AuditData;
 class OpportunityController extends AbstractController
 {
     public function index(RequestInterface $request, ResponseInterface $response)
     {
+
+       foreach ($request->all() as $key => $value) {
+//           var_dump(gettype($key));
+
+           if($key == "audit" && is_array($value)) {
+
+              $idAction =  AuditAction::create($value);
+              $actionId = (int) $idAction->getAttribute("id");
+
+           }
+
+           var_dump($actionId);
+           if($key != "audit") {
+
+//               var_dump(gettype($actionId));
+//               var_dump($actionId);
+//               var_dump($actionId > 0);
+               if($actionId)
+               {
+                   var_dump('Acessou o IF');
+               }
+//              $data = ["key" => $key, "value" => $value, "audit_action_id" => $actionId];
+//               AuditData::create($data);
+           }
+       }
+
+
+//        var_dump($response);
 //        $audit  = new AuditAction();
 //        $audit->user_id = $request->user_id;
 //        $audit->type = $request->type;
@@ -24,6 +52,11 @@ class OpportunityController extends AbstractController
 //       $audit->save();
 //       return $request->all();
 //        Db::table('audit_action')->insert($request->all());
-           return AuditAction::create($request->all());
+//           return AuditAction::create($request->all());
+    }
+
+    public function getIdAction($id)
+    {
+        var_dump($id);
     }
 }
---
+++ hyperf-skeleton/app/Model/AuditData.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+
+
+/**
+ */
+class AuditData extends Model
+{
+    /**
+     * The table associated with the model.
+     */
+    protected ?string $table = 'audit_data';
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected array $fillable = ['id', 'key', 'value', 'audit_action_id'];
+
+    /**
+     * The attributes that should be cast to native types.
+     */
+    protected array $casts = [];
+}
