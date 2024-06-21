7a81f9faa010ba23d3c9390d70bbe045a88107c9
Junior-Shyko <franciscaoanto@gmail.com>
2024-06-21 12:04:19 -0300

    :construction: Comparando se os valores são diferentes

--- hyperf-skeleton/app/Controller/OpportunityController.php
+++ hyperf-skeleton/app/Controller/OpportunityController.php
@@ -8,51 +8,43 @@ use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\HttpServer\Contract\ResponseInterface;
 use App\Model\AuditAction;
 use App\Model\AuditData;
+use Hyperf\DbConnection\Db;
 class OpportunityController extends AbstractController
 {
     public function index(RequestInterface $request, ResponseInterface $response)
     {
+       $actionId = 0;
+       $arrayEntityAuditData = [];
+       $requestMethod = $request->all();
 
-       foreach ($request->all() as $key => $value) {
-//           var_dump(gettype($key));
+        foreach ($requestMethod as $keyRequestMethod => $valueRequestMethod) {
+//            var_dump($keyRequestMethod);
+            if($keyRequestMethod == "audit" && is_array($valueRequestMethod)) {
+//                $idAction =  AuditAction::create($valueRequestMethod);
+//                $actionId = $idAction->getAttribute("id");
+                $actionId = 58;
+            }
 
-           if($key == "audit" && is_array($value)) {
-
-              $idAction =  AuditAction::create($value);
-              $actionId = (int) $idAction->getAttribute("id");
-
-           }
-
-           var_dump($actionId);
-           if($key != "audit") {
-
-//               var_dump(gettype($actionId));
-//               var_dump($actionId);
-//               var_dump($actionId > 0);
-               if($actionId)
-               {
-                   var_dump('Acessou o IF');
-               }
-//              $data = ["key" => $key, "value" => $value, "audit_action_id" => $actionId];
-//               AuditData::create($data);
-           }
-       }
+        }
+        foreach ($requestMethod as $keyAction => $valueAction) {
 
 
-//        var_dump($response);
-//        $audit  = new AuditAction();
-//        $audit->user_id = $request->user_id;
-//        $audit->type = $request->type;
-//        $audit->action = $request->action;
-//        $audit->message = $request->message;
-////        foreach($request->all() as $key => $value) {
-////            var_dump($key);
-////            $audit->$key = $value;
-////        }
-//       $audit->save();
-//       return $request->all();
-//        Db::table('audit_action')->insert($request->all());
-//           return AuditAction::create($request->all());
+            $entityAuditData = AuditData::where('audit_action_id', 1)->get();
+            $arrayEntityAuditData = $entityAuditData->toArray();
+
+        foreach ($arrayEntityAuditData as $keyarrayEntityAuditData => $valuearrayEntityAuditData) {
+
+            if( ($valuearrayEntityAuditData["key"] == $keyAction) && ($valuearrayEntityAuditData["value"] !== $valueAction) )
+            {
+                var_dump($valueAction);
+                die;
+            }
+        }
+
+
+        }
+//
+
     }
 
     public function getIdAction($id)
